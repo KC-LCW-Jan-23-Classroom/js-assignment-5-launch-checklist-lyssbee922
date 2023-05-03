46a5aa2ac45929a472459bcfe3350f046b4918f5
unknown <elyssa.bezner@gmail.com>
2023-05-02 20:04:51 -0500

    trying

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse = myFetch()
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
-       let finalDestination = pickPlanet(listedPlanets)
-       missionTarget.innerHTML = addDestinationInfo(finalDestination);
+       let finalDestinationIndex = pickPlanet(listedPlanets);
+       let finalDestination = listedPlanets[finalDestinationIndex];
+       let missionInfo = addDestinationInfo(document, finalDestination.name, finalDestination.diameter, finalDestination.star, finalDestination.distance, finalDestination.moons, finalDestination.image);
+       document.getElementById("missionTarget").innerHTML = missionInfo;
    })
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -4,16 +4,16 @@ require('isomorphic-fetch');
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
 
     const missionTarget = document.getElementById("missionTarget");
-    return `
-    <h2>Mission Destination</h2>
-    <ol>
-        <li>Name: ${name} </li>
-        <li>Diameter: ${diameter}</li>
-        <li>Star: ${star} </li>
-        <li>Distance from Earth: ${distance} </li>
-        <li>Number of Moons: ${moons} </li>
+    return missionTarget.innerHTML = `
+        <h2>Mission Destination</h2>
+        <ol>
+            <li>Name: ${name} </li>
+            <li>Diameter: ${diameter}</li>
+            <li>Star: ${star} </li>
+            <li>Distance from Earth: ${distance} </li>
+            <li>Number of Moons: ${moons} </li>
         </ol>
-        <img src="${imageUrl}}">
+        <img src="${imageUrl}">
     `;
   
 }
@@ -52,11 +52,11 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     fuelLevel = document.getElementById("fuelStatus");
     cargoLevel = document.getElementById("cargoStatus");
 
-    let pilotName = Chris;
-    let copilotName = Blake;
+    let pilotName = 'Chris';
+    let copilotName = 'Blake';
 
-    document.getElementbyId("pilotStatus").innerHTML = `Pilot ${pilotName} is ready for launch`;
-    document.getElementbyId("copilotStatus").innerHTML= `Co-pilot ${copilotName} is ready for launch`;
+    document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName} is ready for launch`;
+    document.getElementById("copilotStatus").innerHTML= `Co-pilot ${copilotName} is ready for launch`;
 
     if (fuelLevel <10000) {
         faultyItems.style.visibility = "visible"
@@ -69,7 +69,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         launchStatus.innerHTML = "Shuttle not ready for launch.";
         launchStatus.style.color = "red";
     } else {
-        faultyItems.style.visibility = "hidden";
+        faultyItems.style.visibility = 'hidden';
         launchStatus.innerHTML = "Shuttle is ready for launch";
         launchStatus.style.color = "green"
     }  
@@ -86,10 +86,8 @@ async function myFetch() {
 }
 
 function pickPlanet(planets) {
-    planets = ["Tatooine", "Pern", "Saturn/Titan", "Mars", "K2-18b", "Jupiter/Europa" ];
-
     let randomIndex = Math.floor(Math.random()*planets.length);
-    return Number(randomIndex);
+    return planets[randomIndex];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
